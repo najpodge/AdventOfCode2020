9b11240de2f5c0fc5621579bb2a46a61433d91c6
Najla Elmachtoub <nelmachtoub@nelmachtoub.cloudvpn.etsy.com>
2020-12-01 19:18:08 -0500

    Day 1 solution for Advent of Code

---
+++ day1.py
@@ -0,0 +1,25 @@
+def sum2entries(entries):
+    for i in range(len(entries) - 1):
+        for j in range(i + 1, len(entries)):
+            if (entries[i] + entries[j] == 2020):
+                return entries[i] * entries[j]
+
+def sum3entries(entries):
+    for i in range(len(entries) - 2):
+        for j in range(i + 1, len(entries) - 1):
+            for k in range (j + 1, len(entries)):
+                if (entries[i] + entries[j] + entries[k] == 2020):
+                    return entries[i] * entries[j] * entries[k]
+
+def main():
+    with open('input1.txt', 'r') as input:
+        entries = [int(line.strip()) for line in input]
+
+    print("Part 1 answer: ", sum2entries(entries))
+    print("Part 2 answer: ", sum3entries(entries))
+
+if __name__ == "__main__":
+    main()
+
+#Part 1 answer:  1020084
+#Part 2 answer:  295086480
