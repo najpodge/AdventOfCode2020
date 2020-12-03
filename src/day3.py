a6b101142c88ea01246d1851df8afd5a8376198b
Najla Elmachtoub <nelmachtoub@7101.fios-router.home>
2020-12-03 00:52:18 -0500

    AoC day 3 solution

---
+++ src/day3.py
@@ -0,0 +1,24 @@
+def numTrees(map, right, down):
+    numTrees = 0
+    x = 0
+    y = 0
+    width = len(map[0])
+    while y < len(map):
+        numTrees += 1 if map[y][x] == '#' else 0
+        y += down
+        x += right
+        x = x - width if x >= width else x
+    return numTrees
+
+def main():
+    with open('input/input3.txt', 'r') as input:
+        map = [line.strip() for line in input]
+    
+    part1 = numTrees(map, 3, 1)
+    parttwo = numTrees(map, 1, 1) * part1 * numTrees (map, 5, 1) * numTrees(map, 7, 1) * numTrees(map, 1, 2)
+
+    print("Part 1 answer: ", part1)
+    print("Part 2 answer: ", parttwo)
+
+if __name__ == "__main__":
+    main()
